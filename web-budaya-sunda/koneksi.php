4d118478e5fe8649752bdb5db60b1ab953019156
lnell19 <159572639+lnell19@users.noreply.github.com>
2025-10-25 00:30:28 +0700

    add koneksi.php files

---
+++ web-budaya-sunda/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+// Connection details
+$servername = "mysql";
+$username = "root";
+$password = "root";
+$dbname = "sunda";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
